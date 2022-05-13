8f0ccd322335322cd5909ed718e8c75e437a17c4
Plopmenz <plopmenz@gmail.com>
2022-05-13 10:30:08 +0200

    done

---
+++ IDF/IDF/Program.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.Data.Sqlite;
+using System;
+
+namespace IDF
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var connection = new SqliteConnection("Data Source=hello.db"))
+            {
+                connection.Open();
+
+                var command = connection.CreateCommand();
+                command.CommandText =
+                @"
+        SELECT name
+        FROM user
+        WHERE id = $id
+    ";
+                command.Parameters.AddWithValue("$id", "lol");
+
+                using (var reader = command.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        var name = reader.GetString(0);
+
+                        Console.WriteLine($"Hello, {name}!");
+                    }
+                }
+            }
+        }
+    }
+}
